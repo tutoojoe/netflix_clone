85255e0cef1bfb1ceddc7e7b8632a34add88888d
tutoojoe <tutoojoephilip@gmail.com>
2022-03-01 09:36:25 +0530

    Banner  position corrected

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -5,42 +5,39 @@ import { API_KEY, imageUrl } from "../../constants/constants";
 import axios from "../../axios";
 
 const Banner = () => {
-  const [movieList, setMovieList] = useState([]);
+  const [bannerMovie, setBannerMovie] = useState([]);
 
   useEffect(() => {
     axios
       .get(`trending/all/week?api_key=${API_KEY}&language=en-US`)
       .then((response) => {
         console.log(response.data.results[0]);
-        setMovieList(response.data.results[0]);
+        setBannerMovie(response.data.results[0]);
       });
   }, []);
-  // console.log(movieList);
+  // console.log(bannerMovie);
 
   return (
-    
-      <div
-        style={{
-          backgroundImage: `url(${
-            movieList ? imageUrl + movieList.backdrop_path : ""
-          })`,
-        }}
-      >
-
+    <div
+      style={{
+        backgroundImage: `url(${
+          bannerMovie ? imageUrl + bannerMovie.backdrop_path : ""
+        })`, backgroundRepeat:"no-repeat", backgroundSize:"cover",
+      }}
+    >
       <div className={classes.banner}>
         <div className={classes.content}>
-          <h1 className={classes.title}>{movieList ? movieList.title : ""}</h1>
+          <h1 className={classes.title}>{bannerMovie ? bannerMovie.title : ""}</h1>
           <div className={classes.banner_buttons}>
             <button className={classes.button}>Watch now</button>
             <button className={classes.button}>My Playlist</button>
           </div>
           <h1 className={classes.description}>
-            {movieList ? movieList.overview : ""}{" "}
+            {bannerMovie ? bannerMovie.overview : ""}{" "}
           </h1>
         </div>
-        </div>
-        </div>
-  
+      </div>
+    </div>
   );
 };
 
